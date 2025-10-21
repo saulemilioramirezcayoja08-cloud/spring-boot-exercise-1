3fc07f1004bd95c39d1f9f606581648dd49affc1
saulemilioramirezcayoja08-cloud <saulemilioramirezcayoja08@gmail.com>
2025-10-21 17:55:35 -0400

    refactor

---
+++ src/main/java/com/exercise_1/modules/catalog/controller/WarehouseController.java
@@ -0,0 +1,4 @@
+package com.exercise_1.modules.catalog.controller;
+
+public class WarehouseController {
+}
---
+++ src/main/java/com/exercise_1/modules/catalog/service/WarehouseService.java
@@ -0,0 +1,4 @@
+package com.exercise_1.modules.catalog.service;
+
+public class WarehouseService {
+}
